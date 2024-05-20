ba07a6be5b416cf98984d780789e88834ad291d1
MatsuBunp <matsuse.bunpei.890@s.kyushu-u.ac.jp>
2024-05-20 11:09:17 +0900

    [clean] remove test.py
    Because no more need for test.py

--- waerden/test.py
+++
@@ -1,8 +0,0 @@
-# Example to scan an integer and a float
-input_string = input("Enter an integer and a float: ")
-values = input_string.split()
-
-# Convert values to appropriate types
-integer_value = values[0]
-
-print(f"Integer: {integer_value}")
